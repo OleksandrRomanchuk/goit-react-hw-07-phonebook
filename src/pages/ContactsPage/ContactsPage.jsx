dd1182384d5f21148918741e1d4d851a9631b489
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-11 13:04:38 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,12 +1,12 @@
+import ContactList from 'shared/components/ContactList/ContactList';
+import Notification from 'shared/components/Notification/Notification';
+import Loader from 'shared/components/Loader/Loader';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
-import { fetchContacts } from 'redux/operations';
+import { selectContacts } from 'redux/contacts/contactsSelectors';
+import { fetchContacts } from 'redux/contacts/contactsOperations';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import ContactList from 'components/ContactList/ContactList';
-import Notification from 'components/Notification/Notification';
-import Loader from 'components/Loader/Loader';
 
 import { LeftContainer, RigthContainer } from './ContactsPage.styled';
 
