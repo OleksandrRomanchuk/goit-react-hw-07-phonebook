5d9db820048df62ef431911f01a5d47fbf3a88c6
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-11 13:03:31 +0200

    update AppNavigation component

---
+++ src/shared/components/AppNavigation/AppNavigation.styled.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const AppNav = styled.nav`
+	${({ ongroupsPage }) => ongroupsPage && 'padding-left: 378px'}
+`;
+
+export const List = styled.ul`
+	display: flex;
+	gap: 20px;
+`;
+
+export const PageLink = styled(NavLink)`
+	padding: 15px 10px;
+
+	text-decoration: none;
+
+	transition: color 300ms ease-in-out;
+
+	:hover,
+	:focus {
+		color: rgba(241, 178, 17, 1);
+	}
+
+	&.active {
+		color: rgba(42, 153, 159, 1);
+	}
+`;
